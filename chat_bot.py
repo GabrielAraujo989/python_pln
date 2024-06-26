fec17faaf2e03ba341bb6425245087557a1df2e2
Gabriel Araújo <111188805+GabrielAraujo989@users.noreply.github.com>
2024-06-25 21:13:06 -0300

    Update chat_bot.py

--- chat_bot.py
+++ chat_bot.py
@@ -26,10 +26,9 @@ def encontrar_resposta(pergunta, perguntas_respostas, limiar_distancia=5):
 
 if __name__ == "__main__":
   perguntas_respostas = carregar_perguntas("perguntas.txt")
-  limiar_distancia = int(input("Digite o limiar de distância para considerar uma pergunta semelhante:"))
-  while True:
-    pergunta = input("Faça uma pergunta:").lower()
-    if pergunta == "sair":
-      break
-    resposta = encontrar_resposta(pergunta, perguntas_respostas, limiar_distancia)
-    print("Resposta:", resposta)
+  limiar_distancia = 10
+  
+  pergunta = "Quem é voce:"
+   
+  resposta = encontrar_resposta(pergunta, perguntas_respostas, limiar_distancia)
+  print("Resposta:", resposta)
